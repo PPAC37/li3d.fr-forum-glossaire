518f536af45ca0ed12706a8d458c0ed665d6c480
q6 <q6@q6-pc>
2022-12-28 15:36:01 +0100

    ajout base vrac projet Maven (Java)

---
+++ src/main/java/com/ppac37/EText2022/MainDemo.java
@@ -0,0 +1,16 @@
+package com.ppac37.EText2022;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author q6
+ */
+public class MainDemo {
+    public static void main(String[] args) {
+        System.out.println("Coucou !");
+    }
+}
